dd98406c5412cb4d71b8cd98fcb9869d3163259d
Gaëtan Coton <gaetan.coton@outlook.com>
2021-03-15 20:18:45 +0100

    project init

---
+++ src/be/iccbxl/poo/MainApplication1.java
@@ -0,0 +1,14 @@
+package be.iccbxl.poo;
+
+public class MainApplication1 {
+
+	public static void main(String[] args) {
+		String[] membres = {};
+		String[] livres = {};
+		int[] emprunts = {};
+		
+		System.out.println("Hello, nice to meet you !");
+
+	}
+
+}
